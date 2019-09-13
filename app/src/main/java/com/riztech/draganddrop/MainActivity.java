414004643f27ee916c73e0091439b840304264e5
Rizwan <riztech.dev@gmail.com>
2019-09-13 08:37:28 +0530

    Adeed changes

--- app/src/main/java/com/riztech/draganddrop/MainActivity.java
+++ app/src/main/java/com/riztech/draganddrop/MainActivity.java
@@ -1,32 +1,58 @@
 package com.riztech.draganddrop;
 
-import android.app.Activity;
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity {
-
-    private Context context;
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout linLayTarget;
+    private ViewGroup rootLayout;
+    private int _xDelta, _yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = this;
-        LinearLayout llContainerMain = (LinearLayout) findViewById(R.id.llMainContainer);
-        DragDropView dragDropView = new DragDropView(context);
-        dragDropView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-        ImageView ivTest = new ImageView(context);
-        ivTest.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_launcher_foreground));
-        dragDropView.AddDraggableView(ivTest, 50, 50, RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        llContainerMain.addView(dragDropView);
-
-
-
+        rootLayout = findViewById(R.id.view_root);
+        linLayTarget = findViewById(R.id.linLayTarget);
+        linLayTarget.setOnTouchListener(new ChoiceTouchListener());
     }
 
+    class ChoiceTouchListener implements View.OnTouchListener {
+
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+
+
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                   /* MyDragShadowBuilder myDragShadowBuilder = new MyDragShadowBuilder(v);
+                    v.startDrag(null, myDragShadowBuilder, null, 0);*/
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
+                        layoutParams.leftMargin = X -_xDelta;
+                        layoutParams.topMargin = Y - _yDelta;
+                        layoutParams.rightMargin = -250;
+                        layoutParams.bottomMargin = -250;
+                        v.setLayoutParams(layoutParams);
+                        break;
+
+            }
+
+            return false;
+        }
+    }
 }
---
+++ app/src/main/java/com/riztech/draganddrop/MyDragShadowBuilder.java
@@ -0,0 +1,29 @@
+package com.riztech.draganddrop;
+
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+
+public class MyDragShadowBuilder extends DragShadowBuilder {
+
+    public MyDragShadowBuilder(View view) {
+        super(view);
+    }
+
+    @Override
+    public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+        // get width, height
+        int width = getView().getWidth();
+        int height = getView().getHeight();
+
+        // set shadow
+        shadowSize.set(width, height);
+        shadowTouchPoint.set(width/2, height/2);
+    }
+
+    @Override
+    public void onDrawShadow(Canvas canvas) {
+        super.onDrawShadow(canvas);
+    }
+}
